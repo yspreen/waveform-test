ea4f5c308863d0ebb2b52912f679835f03b4a808
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2022-11-13 11:28:43 -0500

    Initial Commit

---
+++ waveformtest/waveformtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  waveformtestApp.swift
+//  waveformtest
+//
+//  Created by Nick on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct waveformtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
