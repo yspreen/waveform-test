63e36f52458bbbdec31eaac93c4a611f023850ba
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2022-11-13 11:36:46 -0500

    Init.

--- waveformtest/ContentView.swift
+++ waveformtest/ContentView.swift
@@ -6,17 +6,51 @@
 //
 
 import SwiftUI
+import DSWaveformImageViews
+
+let path = Bundle.main.path(forResource: "tmp", ofType: "m4a")!
+let url = URL(fileURLWithPath: path)
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+			clipped
+			scaled
         }
         .padding()
+		.frame(maxWidth: .infinity)
+		.frame(maxHeight: .infinity)
+		.background(Color.gray)
     }
+
+	var clipped: some View {
+		WaveformView(
+			audioURL: url,
+			configuration: .init(
+				size: .init(width: 300, height: 50),
+				style: .striped(.init(
+					color: .blue, width: 3, lineCap: .round
+				))
+			)
+		)
+		.frame(width: 300, height: 50)
+		.background(Color.white)
+	}
+
+	var scaled: some View {
+		WaveformView(
+			audioURL: url,
+			configuration: .init(
+				size: .init(width: 300, height: 50),
+				style: .striped(.init(
+					color: .blue, width: 3, lineCap: .round
+				)),
+				verticalScalingFactor: 0.5
+			)
+		)
+		.frame(width: 300, height: 50)
+		.background(Color.white)
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
